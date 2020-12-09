1a7dc2ab2338922fbfe4f5de9f02a4e2824887a7
OscarCanongo <a01730443@itesm.mx>
2020-12-08 18:49:20 -0600

    Basic python

---
+++ Basic Python/input.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+age_in_dog_years = age * 7
+
+print(f"{name} your are {age_in_dog_years} ind dog years. Woof!")
---
+++ Basic Python/strings.py
@@ -0,0 +1,10 @@
+#Strings
+
+kayne_quote = """My gratest pain in life is that 
+I will never be able to see myself perform in life"""
+
+print (kayne_quote)
+
+hamilton_quote = """Well, the word got around, "This kid is insane, man." """
+
+print(hamilton_quote)
