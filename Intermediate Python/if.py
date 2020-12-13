501c532c016c588a35118896dd053c2939e23728
OscarCanongo <oscarcanongo@outlook.com>
2020-12-13 10:15:16 -0600

    Intermediate Python

--- Intermediate Python/if.py
+++ Intermediate Python/if.py
@@ -1,10 +1,12 @@
 answer = input("Do you want to hear a joke? ")
 
-if answer == "Yes":
+affirmative_responses = ["yes", "y"]
+negative_responses = ["no", "n"]
+
+if answer.lower() in affirmative_responses:
     print("I'm against picketing, but I don't know how to show it.")
-elif answer == "No":
+elif answer.lower() in negative_responses:
     print("Fine.")    
 else:
     print("I don't understand.")
-
 
